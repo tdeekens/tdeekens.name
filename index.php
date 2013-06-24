d39c6f35558cf7eb5d4f4838a7fb42d88f482a6a
Tobias Deekens <tobias.deekens@gmail.com>
2013-06-24 11:29:41 +0200

    Adds menu blueprint.

--- index.php
+++ index.php
@@ -22,72 +22,83 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
+        <div class="wrap-outer">
+            <div class="navigation">
+                <ul>
+                    <li><a href="<?php echo baseUri; ?>about">It's about</a></li>
+                    <li><a href="<?php echo baseUri; ?>posts">posts</a></li>
+                    <li><a href="<?php echo baseUri; ?>music">and music</a></li>
+                    <li><a href="<?php echo baseUri; ?>read">with reading</a></li>
+                </ul>
+            </div>
+            <div class="content-body">
+                <?php
+                    require_once 'php/components/autoload.php';
 
-        <?php
-            require_once 'php/components/autoload.php';
+                    $pageDirectory = "pages/";
+                    $router = new \Slim\Slim();
 
-            $pageDirectory = "pages/";
-            $router = new \Slim\Slim();
+                    $router->get('/', function() use ($pageDirectory) {
+                        $file = $pageDirectory . "about.php";
 
-            $router->get('/', function() use ($pageDirectory) {
-                $file = $pageDirectory . "about.php";
+                        require_once $file;
 
-                require_once $file;
+                        $about = new tdeekens\blog\About();
 
-                $about = new tdeekens\blog\About();
+                        $about->about();
+                    });
 
-                $about->about();
-            });
+                    $router->get('/posts/', function() use ($pageDirectory) {
+                        $file = $pageDirectory . "posts.php";
 
-            $router->get('/posts/', function() use ($pageDirectory) {
-                $file = $pageDirectory . "posts.php";
+                        require_once $file;
 
-                require_once $file;
+                        $posts = new tdeekens\blog\Posts();
 
-                $posts = new tdeekens\blog\Posts();
+                        $posts->listAll();
+                    });
 
-                $posts->listAll();
-            });
+                    $router->get('/post/:file', function($name) use ($pageDirectory) {
+                        $file = $pageDirectory . "posts.php";
 
-            $router->get('/post/:file', function($name) use ($pageDirectory) {
-                $file = $pageDirectory . "posts.php";
+                        require_once $file;
 
-                require_once $file;
+                        $posts = new tdeekens\blog\Posts();
 
-                $posts = new tdeekens\blog\Posts();
+                        $posts->showOne($name);
+                    });
 
-                $posts->showOne($name);
-            });
+                    $router->get('/music/', function() use ($pageDirectory) {
+                        $file = $pageDirectory . "music.php";
 
-            $router->get('/music/', function() use ($pageDirectory) {
-                $file = $pageDirectory . "music.php";
+                        require_once $file;
 
-                require_once $file;
+                        $music = new tdeekens\blog\Music();
 
-                $music = new tdeekens\blog\Music();
+                        $music->listAll();
+                    });
 
-                $music->listAll();
-            });
+                    $router->get('/read/', function($file) use ($pageDirectory) {
+                        $file = $pageDirectory . "read.php";
 
-            $router->get('/read/', function($file) use ($pageDirectory) {
-                $file = $pageDirectory . "read.php";
+                        require_once $file;
 
-                require_once $file;
+                        $posts = new tdeekens\blog\Read();
+                    });
 
-                $posts = new tdeekens\blog\Read();
-            });
+                    $router->get('/file/:name', function($name) use ($router, $pageDirectory) {
+                        $file = $pageDirectory . "files.php";
 
-            $router->get('/file/:name', function($name) use ($router, $pageDirectory) {
-                $file = $pageDirectory . "files.php";
+                        require_once $file;
 
-                require_once $file;
+                        $files = new tdeekens\blog\Files($router);
 
-                $files = new tdeekens\blog\Files($router);
+                        $files->load($name);
+                    });
 
-                $files->load($name);
-            });
-
-            $router->run();
-        ?>
+                    $router->run();
+                ?>
+            </div>
+        </div>
     </body>
 </html>
