92eb4074330538d4cdef39924a6d89d720cca738
Tobias Deekens <tobias.deekens@gmail.com>
2013-09-09 23:04:20 +0200

    Fixes piwik-tracking.

--- app/javascripts/piwik-tracking.js
+++ app/javascripts/piwik-tracking.js
@@ -1,5 +1,4 @@
-<!-- Piwik -->
-<script type="text/javascript">
+(function() {
   var _paq = _paq || [];
   _paq.push(["trackPageView"]);
   _paq.push(["enableLinkTracking"]);
@@ -11,5 +10,4 @@
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
   })();
-</script>
-<!-- End Piwik Code -->
+})();
