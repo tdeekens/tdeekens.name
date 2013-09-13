bd4e5da8326f6b87a0bbfe7d05b2000df7ff21a4
Tobias Deekens <tobias.deekens@gmail.com>
2013-09-13 14:11:48 +0200

    Fixes flushcache shell-script.

--- flushcaches.sh
+++ flushcaches.sh
@@ -1,21 +1,21 @@
 echo "# Flushing all projec's caches..."
 
-rm -r app/coffeescript/_cache/*
+rm -rf app/coffeescript/_cache/*
 echo "1.) Flushed coffeescript cache"
 
-rm -r app/javascripts/_cache/*
+rm -rf app/javascripts/_cache/*
 echo "1.) Flushed javascript cache"
 
-rm -r app/img/_cache/*
+rm -rf app/img/_cache/*
 echo "1.) Flushed image cache"
 
-rm -r app/styles/_cache/*
+rm -rf app/styles/_cache/*
 echo "1.) Flushed styles cache"
 
-rm -r app/fonts/_cache/*
+rm -rf app/fonts/_cache/*
 echo "1.) Flushed fonts cache"
 
-rm -r app/markdown/_cache/*
+rm -rf app/markdown/_cache/*
 echo "1.) Flushed markdown cache"
 
 echo "# Done: Caches fresh!"
