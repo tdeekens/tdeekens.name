5120d0aadb0b3242f8b09e7e311d58c2515d8bfd
Tobias Deekens <tobias.deekens@gmail.com>
2013-09-13 14:31:39 +0200

    Adds cache-prefix to flushcaches script.

--- flushcaches.sh
+++ flushcaches.sh
@@ -1,21 +1,28 @@
-echo "# Flushing all project's caches..."
+if [[ "$1" != "" ]]
+   then
+      CACHE_PREFIX=$1
+   else
+      CACHE_PREFIX="app"
+fi
 
-rm -rf app/coffeescript/_cache/*
+echo "# Flushing all project's caches ($CACHE_PREFIX)"
+
+rm -rf $CACHE_PREFIX/coffeescript/_cache/*
 echo "1.) Flushed coffeescript cache"
 
-rm -rf app/javascripts/_cache/*
+rm -rf $CACHE_PREFIX/javascripts/_cache/*
 echo "2.) Flushed javascript cache"
 
-rm -rf app/img/_cache/*
+rm -rf $CACHE_PREFIX/img/_cache/*
 echo "2.) Flushed image cache"
 
-rm -rf app/styles/_cache/*
+rm -rf $CACHE_PREFIX/styles/_cache/*
 echo "3.) Flushed styles cache"
 
-rm -rf app/fonts/_cache/*
+rm -rf $CACHE_PREFIX/fonts/_cache/*
 echo "4.) Flushed fonts cache"
 
-rm -rf app/markdown/_cache/*
+rm -rf $CACHE_PREFIX/markdown/_cache/*
 echo "5.) Flushed markdown cache"
 
 echo "# Done: Caches fresh!"
