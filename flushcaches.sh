0db2f2161cbd4ad08941886bf80a062845d95dcf
Tobias Deekens <tobias.deekens@gmail.com>
2013-09-13 14:19:54 +0200

    Updates flushcaches script.

--- flushcaches.sh
+++ flushcaches.sh
@@ -1,21 +1,21 @@
-echo "# Flushing all projec's caches..."
+echo "# Flushing all project's caches..."
 
 rm -rf app/coffeescript/_cache/*
 echo "1.) Flushed coffeescript cache"
 
 rm -rf app/javascripts/_cache/*
-echo "1.) Flushed javascript cache"
+echo "2.) Flushed javascript cache"
 
 rm -rf app/img/_cache/*
-echo "1.) Flushed image cache"
+echo "2.) Flushed image cache"
 
 rm -rf app/styles/_cache/*
-echo "1.) Flushed styles cache"
+echo "3.) Flushed styles cache"
 
 rm -rf app/fonts/_cache/*
-echo "1.) Flushed fonts cache"
+echo "4.) Flushed fonts cache"
 
 rm -rf app/markdown/_cache/*
-echo "1.) Flushed markdown cache"
+echo "5.) Flushed markdown cache"
 
 echo "# Done: Caches fresh!"
