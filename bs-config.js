1953200cc7ca6ee9532bf057a1e5aefb00bdf0e3
Tobias Deekens <nerd@tdeekens.name>
2014-06-20 11:22:21 +0200

    Adds browser-sync configuration.

---
+++ bs-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+    files: ["app/styles/**.scss", "app/coffeescript/**.coffee", "app/javascripts/**.js"],
+
+    exclude: false,
+
+    server: false,
+
+    proxy: "tdeekens.local",
+
+    startPath: "tdeekens.name",
+
+    debugInfo: true,
+
+    ghostMode: {
+        clicks: true,
+        links: true,
+        forms: true,
+        scroll: true
+    },
+
+    open: true,
+
+    xip: false,
+
+    timestamps: true,
+
+    fileTimeout: 1000,
+
+    injectChanges: true,
+
+    scrollProportionally: true,
+
+    scrollThrottle: 0,
+
+    notify: true,
+
+    host: null,
+
+    excludedFileTypes: [],
+
+    reloadDelay: 0
+
+};
