b099c81359c36b163c47d8698dad3aed98315585
tobi <tobi@brandwatch.com>
2016-06-26 14:19:30 +0200

    Add thinking section to about page.

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -13,11 +13,15 @@ class About extends Controller
 
    public static function index()
    {
-      $posts         = static::readIndex('Posts', true);
-      $music         = static::readIndex('Music', true);
-      $books         = static::readIndex('Books');
-      $recentPosts   = Arrays::from($posts)->filter(function($post) {
+      $posts          = static::readIndex('Posts', true);
+      $thoughts       = static::readIndex('Thoughts', true);
+      $music          = static::readIndex('Music', true);
+      $books          = static::readIndex('Books');
+      $recentPosts    = Arrays::from($posts)->filter(function($post) {
          return $post->draft === null || $post->draft !== true;
+      })->first(2)->obtain();
+      $recentThoughts = Arrays::from($thoughts)->filter(function($thought) {
+         return $thought->draft === null || $thought->draft !== true;
       })->first(2)->obtain();
 
       return static::renderMustache([
@@ -25,6 +29,7 @@ class About extends Controller
          'credits'   => Asset::markdown('credits.md', [], true),
          'songs'     => Arrays::first($music, 2),
          'posts'     => array_merge($recentPosts),
+         'thoughts'  => array_merge($recentThoughts),
          'books'     => Arrays::first($books, 2)
       ], ['path' => './app/views/about/', 'files' => ['about' => 'about']]);
    }
