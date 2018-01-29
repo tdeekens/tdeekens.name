ebc901f093428f1972ff067a908bb3be7b6045b6
tdeekens <nerd@tdeekens.name>
2018-01-29 22:39:07 +0100

    Add first CV draft Sarah

--- app/controllers/CurriculumVitae.php
+++ app/controllers/CurriculumVitae.php
@@ -8,13 +8,18 @@ use Underscore\Types\Arrays;
 
 class CurriculumVitae extends Controller
 {
-   public static function index()
-   {
-      return static::render([], null, 200, 'app/views/curriculum-vitae.html');
-   }
+    public static function index()
+    {
+        return static::render([], null, 200, 'app/views/curriculum-vitae.html');
+    }
 
-   public static function cover()
-   {
-      return static::render([], null, 200, 'app/views/curriculum-vitae.html');
-   }
+    public static function cover()
+    {
+        return static::render([], null, 200, 'app/views/curriculum-vitae.html');
+    }
+
+    public static function grosche()
+    {
+        return static::render([], null, 200, 'app/views/curriculum-vitae.html');
+    }
 }
