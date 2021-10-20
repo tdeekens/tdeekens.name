67c0d8cbee4832d899bc762b65bcedb355f40bdc
tdeekens <tobias.deekens@commercetools.com>
2021-10-20 10:09:42 +0200

    feat: add some tooling

---
+++ components/layout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+type TLayoutProps = {
+  children: React.ReactElement;
+};
+
+function Layout(props: TLayoutProps) {
+  return (
+    <div className="flex justify-center items-center">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="my-8 mx-4 md:mx-16 font-mono xl:text-sm max-w-6xl">
+        {props.children}
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
