e3088810bdb85e3bdd052ab4fed72e3b1854e797
tdeekens <tobias.deekens@commercetools.com>
2021-10-21 21:38:18 +0200

    feat: add breadcrumbs

--- components/internal-link.tsx
+++ components/internal-link.tsx
@@ -3,14 +3,24 @@ import Link from 'next/link';
 type TInternalLinkProps = {
   children: React.ReactNode;
   href: string;
+  underline?: boolean;
 };
 
 function InternalLink(props: TInternalLinkProps) {
   return (
     <Link href={props.href} passHref>
-      <a className="underline hover:no-underline">{props.children}</a>
+      <a
+        className={[props.underline && 'underline', 'hover:no-underline']
+          .filter(Boolean)
+          .join(' ')}
+      >
+        {props.children}
+      </a>
     </Link>
   );
 }
+InternalLink.defaultProps = {
+  underline: true,
+};
 
 export default InternalLink;
