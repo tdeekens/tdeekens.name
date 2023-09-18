8d523affaa69a3f0a9b3932f7b49739f0e2795bf
tdeekens <tobias.deekens@commercetools.com>
2023-09-18 23:55:26 +0200

    refactor: to use pnpm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,3 @@
-function lint(filenames) {
-  return `next lint --fix --file ${filenames
-    .map((file) => file.split(process.cwd())[1])
-    .join(' --file ')}`;
-}
-
 module.exports = {
   '*.md': ['prettier --write --parser markdown'],
   '*.yaml': ['prettier --write --parser yaml'],
@@ -11,5 +5,5 @@ module.exports = {
   '*.js?(x)': ['prettier --write'],
   // # Not released yet https://github.com/vercel/next.js/pull/28042
   //'*.{js?(x), ts?(x)}': lint,
-  '*.{ts,tsx}': ['yarn prettier --write', 'tsc-files --noEmit'],
+  '*.{ts,tsx}': ['pnpm prettier --write', 'tsc-files --noEmit'],
 };
