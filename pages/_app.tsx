299881fe0877743f7f6a7ff26bdc20dd6230a4ce
tdeekens <tobias.deekens@commercetools.com>
2024-08-30 15:08:39 +0200

    fix: integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,12 @@ import Layout from '@components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
+    <>
       <SpeedInsights />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 export default MyApp;
