ea315c046879e8f79cb8edb109c7a63e044021aa
tdeekens <tobias.deekens@commercetools.com>
2026-04-08 22:28:03 +0200

    feat: add blogroll

---
+++ components/blogroll-entry.tsx
@@ -0,0 +1,38 @@
+import Text from '@components/text';
+import ExternalLink from '@components/external-link';
+
+type TBlogrollEntryProps = {
+  title: string;
+  href: string;
+  author: string;
+  teaser: string;
+  tags?: string[];
+};
+
+function BlogrollEntry(props: TBlogrollEntryProps) {
+  return (
+    <article className="flex flex-col space-y-1">
+      <Text.Headline as="h3">
+        <ExternalLink href={props.href}>{props.title}</ExternalLink>
+      </Text.Headline>
+      <div className="flex items-baseline gap-2">
+        <Text.Headline as="h4">by {props.author}</Text.Headline>
+        {props.tags && props.tags.length > 0 && (
+          <div className="flex flex-wrap gap-1">
+            {props.tags.map((tag) => (
+              <span
+                key={tag}
+                className="text-xs px-1.5 py-0.5 rounded-full border border-gray-300 text-gray-600"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+      <Text.Paragraph>{props.teaser}</Text.Paragraph>
+    </article>
+  );
+}
+
+export default BlogrollEntry;
