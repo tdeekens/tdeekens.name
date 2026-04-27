a663205b165fe3d2541cd7819e00f66ee0f4bd9b
tdeekens <tobias.deekens@commercetools.com>
2026-04-27 15:41:54 +0200

    feat: add link headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Link',
+            value:
+              '</sitemap.xml>; rel="index"; type="application/xml", </robots.txt>; rel="service-meta"; type="text/plain"',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
